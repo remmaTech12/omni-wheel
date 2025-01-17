dd6120329df819be994f2fd995250b18563620dc
Remma Matsuda <titech.renma@gmail.com>
2025-01-17 09:45:15 +0900

    separate pin allocation hpp.

---
+++ software/omni_robot/main/include/pin_allocation.hpp
@@ -0,0 +1,17 @@
+#define MOTOR1_ENC_A_PIN 32
+#define MOTOR1_ENC_B_PIN 33
+#define MOTOR1_IN1_PIN 13
+#define MOTOR1_IN2_PIN 12
+
+#define MOTOR2_ENC_A_PIN 35
+#define MOTOR2_ENC_B_PIN 23
+#define MOTOR2_IN1_PIN 26
+#define MOTOR2_IN2_PIN 27
+
+#define MOTOR3_ENC_A_PIN 19
+#define MOTOR3_ENC_B_PIN 18
+#define MOTOR3_IN1_PIN 4
+#define MOTOR3_IN2_PIN 25
+
+#define LED_PIN 2
+#define SW_PIN 34
--- software/omni_robot/main/main.ino
+++ software/omni_robot/main/main.ino
@@ -1,29 +1,12 @@
-#include<Wire.h>
-#include "./include/pid.hpp"
-#include "./include/motor.hpp"
 #include "./include/imu_bmx055.hpp"
+#include "./include/motor.hpp"
+#include "./include/pid.hpp"
+#include "./include/pin_allocation.hpp"
+#include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 #include "BluetoothSerial.h"
-
-#define MOTOR1_ENC_A_PIN 32
-#define MOTOR1_ENC_B_PIN 33
-#define MOTOR1_IN1_PIN 13
-#define MOTOR1_IN2_PIN 12
-
-#define MOTOR2_ENC_A_PIN 35
-#define MOTOR2_ENC_B_PIN 23
-#define MOTOR2_IN1_PIN 26
-#define MOTOR2_IN2_PIN 27
-
-#define MOTOR3_ENC_A_PIN 19
-#define MOTOR3_ENC_B_PIN 18
-#define MOTOR3_IN1_PIN 4
-#define MOTOR3_IN2_PIN 25
-
-#define LED_PIN 2
-#define SW_PIN 34
 
 #define MOTOR_NUM 3
 
